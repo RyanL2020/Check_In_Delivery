3621e9ac62490c63bce62f9d993bbc48dab1fe99
RL2020 <loc.fitness1@gmail.com>
2021-04-19 11:03:56 -0400

    Added to sessions_controller /login

--- check_in_delivery/app/controllers/sessions_controller.rb
+++ check_in_delivery/app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
 
 
-   
-    end
+    get "/login" do
+        erb :"/sessions/login"
+    end 
+
+    post "/login" do 
+        user = User.find_by(username: params[:username])
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect "/deliveries"
+        else 
+            redirect "/login"
+        end 
+    end 
+
+
+end
